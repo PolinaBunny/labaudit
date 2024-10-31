d62001b6c186606b8ad696fc5870ef0cf96baf1b
Polina <p.salnikova@yandex.ru>
2024-10-31 10:12:57 +1000

    ~ app.py: Методы HEAD и OPTIONS вызывают исключение на роутах на которых их быть не должно

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
-
 # Author: Polina Salnikova
-# Notes:
-#  - Метод OPTIONS включен на всех роутах, т.к. в Flask его отключить нельзя
 
 from flask import Flask, Response, request, abort, render_template
 import hashlib
@@ -13,34 +10,39 @@ app = Flask(__name__)
 def hello_world():
     return '<p>Привет! Это сайт с нескушным дизайном!</p><p><a href="/get">get</a> <a href="/post">post</a> <a href="/head">head</a> <a href="/options">options</a></p>'
 
-@app.route("/get", methods=['GET'])
+@app.route("/get", methods=['GET', 'HEAD', 'OPTIONS'])
 def get_func():
     if request.method == 'GET':                 # Flask очень явно не хочет выпонять требования, 
                                                 # которые ты перед ним ставишь, 
                                                 # поэтому проверку на метод запроса считаю обязательной
         if request.args.get("username"):
-            return render_template('login.html', user='value=' + request.args.get("username"))
+            response = Response(render_template('login.html', user='value=' + request.args.get("username")))
+            response.headers["username"] = request.args.get("username")
+            return response
         return render_template('login.html', user="")
     else:
         abort(405)                              # Метод не поддерживается
 
-@app.route("/post", methods=['POST'])
+@app.route("/post", methods=['POST', 'OPTIONS'])
 def post_func():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if username == "admin" and password: 
-            return render_template("yota.html")
+            response = Response(render_template("yota.html"))
         else: 
-            return render_template("lose.html")
+            response = Response(render_template("lose.html"))
+        response.headers["username"] = username
+        response.headers["password"] = password
+        return response
     else:
         abort(405)
 
-@app.route("/head", methods=['HEAD'])
+@app.route("/head", methods=['HEAD', 'OPTIONS'])
 def head_func():
     if request.method == 'HEAD':
         response = Response()
-        response.headers["Attension"] = "@Slim_Shady, please stand up!"
+        response.headers["Attention"] = "@Slim_Shady, please stand up!"
         return response
     else:
         abort(405)
@@ -48,6 +50,8 @@ def head_func():
 @app.route("/options", methods=['OPTIONS'])
 def options_func():
     if request.method == 'OPTIONS':
-        return "<p>Hello, World!</p>"
+        response = Response("<p>Hello, World!</p>")
+        response.headers["Allow"] = "OPTIONS"
+        return response
     else:
         abort(405)
